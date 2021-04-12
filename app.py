6d0948836fc1c0af6a62fc38d3de9857b4bc82ba
Swapnil Srivastava <62797602+Swapnil4Github@users.noreply.github.com>
2021-04-12 22:21:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # packages required for the project
-from flask import Flask
+from flask import Flask, jsonify
 import spacy
 from spacy import displacy
 import wikipedia
@@ -7,15 +7,15 @@ import wikipedia
 
 nlp = spacy.load('en_core_web_lg')
 
-
 app = Flask(__name__)
 
 @app.route('/<user_input>')
-def main(user_input):
+def mmain(user_input):
     raw_text = user_input.replace(" ", "_")
     complete_content = wikipedia.summary(raw_text)
     docx = nlp(complete_content)
-    return displacy.render(docx,style="ent")
+    d = displacy.render(docx,style="ent")
+    return {"html_data" : d}
 
 if __name__ == "__main__":
     app.run(debug=True)
